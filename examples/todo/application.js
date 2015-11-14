58776460f9b5064586d1f5d7d94f23b8331243b9
Kirill Mokevnin <mokevnin@gmail.com>
2015-11-14 14:29:38 +0300

    update todo app

--- examples/todo/application.js
+++ examples/todo/application.js
@@ -1,5 +1,6 @@
 import Act from "act";
 
+import _ from "lodash";
 const jade = require("jade");
 
 const template = require("raw!./todo.jade"); // fs.readFileSync("./todo.jade");
@@ -7,45 +8,75 @@ const templateFn = jade.compile(template);
 
 class ToDo extends Act.Component {
   getInitialState() {
-    return {currentText: "", tasks: this.ops.tasks || []};
+    return {currentText: "", tasks: this.ops.tasks || {}};
   }
 
-  handleTaskInput(e) {
-    e.preventDefault();
-    // console.log(e.target.value);
-    this.setState({currentText: e.target.value});
-  }
-
-  handleMarkTaskAsCompleted(e) {
-    e.preventDefault();
-    const text = e.target.innerHTML;
-    const tasks = _.without(this.state.tasks, _.trim(text));
-    console.log(tasks);
-    this.setState({tasks: tasks});
+  getTaskClass(bool) {
+    return bool ? "task-completed" : "task-new";
   }
 
   handleAddTask(e) {
     e.preventDefault();
 
-    const tasks = this.state.tasks.slice();
-    tasks.unshift(this.state.currentText);
+    var tasks = _.clone(this.state.tasks);
+    const id = generateId();
+    tasks[id] = {
+      id: id,
+      text: this.state.currentText,
+      completed: false
+    }
     this.setState({currentText: "", tasks: tasks});
   }
 
   render() {
-    console.log(this.state.currentText);
+    // console.log(this.state.tasks);
     const locals = {
       tasks: this.state.tasks,
+      getTaskClass: this.getTaskClass,
       currentText: this.state.currentText
     };
 
     return templateFn(locals);
   }
+
+  handleTaskInput(e) {
+    e.preventDefault();
+
+    this.setState({currentText: e.target.value});
+  }
+
+  handleRemoveTask(e) {
+    e.preventDefault();
+    // console.log(e.target.dataset);
+
+    var tasks = _.clone(this.state.tasks);
+    const id = parseInt(e.target.dataset.id);
+    delete tasks[id];
+    this.setState({tasks: tasks});
+  }
+
+  handleToggleCompleted(e) {
+    e.preventDefault();
+    console.log(e.target.dataset);
+
+    var tasks = _.clone(this.state.tasks);
+    const id = parseInt(e.target.dataset.id);
+    tasks[id]["completed"] = !tasks[id]["completed"];
+    this.setState({tasks: tasks});
+  }
 };
 
-window.component = new ToDo({tasks: ["write new super framework"]});
+window.component = new ToDo({tasks: {1: {id: 1, completed: false, text: "write new super framework"}}});
 
 Act.DOM.render(
     component,
     document.getElementById("container")
 );
+
+var startNumber = 1;
+
+function generateId() {
+  startNumber++;
+  return startNumber;
+}
+
